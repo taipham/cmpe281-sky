2090f2090d696d3dd724ffc82e183a6116a2d14e
Tyler Nicholson <tan.hombre@gmail.com>
2017-10-28 09:56:13 -0700

    added structs and login/out funcs

--- project/main.go
+++ project/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
         "fmt"
-	    "log"
+	"log"
         "gopkg.in/mgo.v2"
         "gopkg.in/mgo.v2/bson"
 )
+
+type Credential struct {
+
+   Username admin
+   Password admin
+
+}
+
+type Song struct {
+
+   ID bson.ObjectId `json:"id" bson:"_id"`
+   Title string
+   Album string
+   Artist string
+
+}
+
+func (db *Database) Login(user, pass string) error
+
+func (db *Database) Logout()
 
 func main() {
 
