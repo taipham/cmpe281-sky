d7d02a056cba584f2b7ed837c0a310070e42eed1
Melodyxman <melodywang531@gmail.com>
2017-11-10 11:34:01 -0800

    users' login info & favors

---
+++ project/usersInfo.js
@@ -0,0 +1,551 @@
+db.usersInfo.insert(
+/* 1 */
+{
+   
+    "first_name" : "Andrew",
+    "last_name" : "William",
+    "user_name" : "AWhero",
+    "password" : "12345678",
+    "age" : 24.0,
+    "city" : "Houston",
+    "music_tags" : [ 
+        "rock", 
+        "r&b", 
+        "country"
+    ],
+    "favor" : [],
+    "lastVist" : ISODate("2017-01-28T09:40:50.615Z")
+}
+
+/* 2 */
+{
+   
+    "first_name" : "Amy",
+    "last_name" : "Branda",
+    "user_name" : "Alphago",
+    "password" : "12345678",
+    "age" : 22.0,
+    "city" : "San Jose",
+    "music_tags" : [ 
+        "rock", 
+        "r&b", 
+        "country"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "Adele",
+            "album" : [ 
+                "21", 
+                "19"
+            ]
+        }, 
+        {
+            "singer" : "cold play",
+            "album" : [ 
+                "Viva la Vida", 
+                "x&y", 
+                "LeftRightLeft"
+            ]
+        }, 
+        {
+            "singer" : "linkin park",
+            "album" : "xero"
+        }
+    ],
+    "lastVist" : ISODate("2017-01-28T09:40:50.615Z")
+}
+
+/* 3 */
+{
+    
+    "first_name" : "Celine",
+    "last_name" : "Dianna",
+    "user_name" : "puppydidi",
+    "password" : "12348765",
+    "age" : 19.0,
+    "city" : "San Jose",
+    "music_tags" : [ 
+        "pop"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "Bruno Mars",
+            "album" : [ 
+                "Doo-wops & hooligans", 
+                "24k magic"
+            ]
+        }, 
+        {
+            "singer" : "taylor swift",
+            "album" : [ 
+                "reputation", 
+                "1989", 
+                "red"
+            ]
+        }, 
+        {
+            "singer" : "justin biber",
+            "album" : [ 
+                "perpose", 
+                "journals"
+            ]
+        }
+    ],
+    "lastVist" : ISODate("2017-11-28T09:40:50.615Z")
+}
+
+/* 4 */
+{
+   
+    "first_name" : "Emma",
+    "last_name" : "Watson",
+    "user_name" : "Hermione",
+    "password" : "1@3$5^7*",
+    "age" : 29.0,
+    "city" : "San Francisco",
+    "music_tags" : [ 
+        "blues", 
+        "jazz"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "lisa ono",
+            "album" : [ 
+                "Naima", 
+                "soul & bossa"
+            ]
+        }, 
+        {
+            "singer" : "taylor swift",
+            "album" : [ 
+                "reputation"
+            ]
+        }, 
+        {
+            "singer" : "Neil Diamond",
+            "album" : [ 
+                "the jazz singer", 
+                "stones", 
+                "melody road"
+            ]
+        }
+    ],
+    "lastVist" : ISODate("2017-11-01T19:40:50.615Z")
+}
+
+/* 5 */
+{
+   
+    "first_name" : "Fiona",
+    "last_name" : "Wade",
+    "user_name" : "princessfiona",
+    "password" : "12345678",
+    "age" : 32.0,
+    "city" : "San Francisco",
+    "music_tags" : [ 
+        "blues", 
+        "jazz", 
+        "pop"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "lisa ono",
+            "album" : [ 
+                "Naima", 
+                "soul & bossa"
+            ]
+        }, 
+        {
+            "singer" : "Koko Taylor",
+            "album" : [ 
+                "old school", 
+                "jump for joy", 
+                "royal blue"
+            ]
+        }
+    ],
+    "lastVist" : ISODate("2017-09-11T19:40:50.615Z")
+}
+
+/* 6 */
+{
+    
+    "first_name" : "George",
+    "last_name" : "Carano",
+    "user_name" : "appleG",
+    "password" : "12345678",
+    "age" : 29.0,
+    "city" : "San Diego",
+    "music_tags" : [ 
+        "rock", 
+        "electronic", 
+        "pop"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "Marconi Union",
+            "song" : [ 
+                "Weightless"
+            ]
+        }, 
+        {
+            "singer" : "Marian Hill",
+            "song" : [ 
+                "Down"
+            ]
+        }, 
+        {
+            "singer" : "Alison Wonderland",
+            "song" : [ 
+                "Happy Palace"
+            ]
+        }, 
+        {
+            "singer" : "cold play",
+            "album" : [ 
+                "Viva la Vida"
+            ],
+            "song" : [ 
+                "Viva la Vida", 
+                "The Scientist", 
+                "Megic"
+            ]
+        }
+    ],
+    "lastVist" : ISODate("2017-10-12T19:40:50.615Z")
+}
+
+/* 7 */
+{
+   
+    "first_name" : "Fabio",
+    "last_name" : "Smith",
+    "user_name" : "fatbro",
+    "password" : "12345678",
+    "age" : 29.0,
+    "city" : "San Diego",
+    "music_tags" : [ 
+        "electronic", 
+        "pop"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "Marconi Union",
+            "song" : [ 
+                "Weightless"
+            ]
+        }, 
+        {
+            "singer" : "Marian Hill",
+            "song" : [ 
+                "Down"
+            ]
+        }, 
+        {
+            "singer" : "Bruno Mars",
+            "album" : [ 
+                "Doo-wops & hooligans"
+            ],
+            "song" : [ 
+                "The Lazy Song", 
+                "Chunky"
+            ]
+        }, 
+        {
+            "singer" : "cold play",
+            "album" : [ 
+                "Viva la Vida"
+            ],
+            "song" : [ 
+                "Viva la Vida", 
+                "The Scientist"
+            ]
+        }
+    ],
+    "lastVist" : ISODate("2017-10-12T19:40:50.615Z")
+}
+
+/* 8 */
+{
+   
+    "first_name" : "Harper",
+    "last_name" : "Jone",
+    "user_name" : "harrypotter",
+    "password" : "12345678",
+    "age" : 19.0,
+    "city" : "New York",
+    "music_tags" : [ 
+        "electronic", 
+        "pop"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "Justin Biber",
+            "album" : [ 
+                "Purpose"
+            ],
+            "song" : [ 
+                "what do you mean", 
+                "I will show you"
+            ]
+        }, 
+        {
+            "singer" : "Marian Hill",
+            "song" : [ 
+                "Down"
+            ]
+        }, 
+        {
+            "singer" : "Bruno Mars",
+            "album" : [ 
+                "Doo-wops & hooligans"
+            ],
+            "song" : [ 
+                "The Lazy Song"
+            ]
+        }, 
+        {
+            "singer" : "cold play",
+            "album" : [ 
+                "Viva la Vida"
+            ],
+            "song" : [ 
+                "Viva la Vida"
+            ]
+        }
+    ],
+    "lastVist" : ISODate("2017-10-20T19:40:50.615Z")
+}
+
+/* 9 */
+{
+    
+    "first_name" : "Justin",
+    "last_name" : "BiberBiber",
+    "user_name" : "Justbib",
+    "password" : "qazwsx123",
+    "age" : 17.0,
+    "city" : "Salt Lake City",
+    "music_tags" : [ 
+        "electronic", 
+        "pop"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "Justin Biber",
+            "album" : [ 
+                "Purpose"
+            ],
+            "song" : [ 
+                "what do you mean", 
+                "I will show you"
+            ]
+        }, 
+        {
+            "singer" : "Bruno Mars",
+            "album" : [ 
+                "Doo-wops & hooligans"
+            ],
+            "song" : [ 
+                "The Lazy Song"
+            ]
+        }, 
+        {
+            "singer" : "cold play",
+            "album" : [ 
+                "Viva la Vida"
+            ],
+            "song" : [ 
+                "Viva la Vida"
+            ]
+        }
+    ],
+    "lastVist" : ISODate("2017-07-20T19:40:50.615Z")
+}
+
+/* 10 */
+{
+   
+    "first_name" : "Kaden",
+    "last_name" : "Jones",
+    "user_name" : "kaden123",
+    "password" : "qazwsx123",
+    "age" : 24.0,
+    "city" : "Salt Lake City",
+    "music_tags" : [ 
+        "rock", 
+        "pop"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "Justin Biber",
+            "album" : [ 
+                "Purpose"
+            ],
+            "song" : [ 
+                "what do you mean", 
+                "I will show you"
+            ]
+        }, 
+        {
+            "singer" : "Bruno Mars",
+            "album" : [ 
+                "Doo-wops & hooligans"
+            ],
+            "song" : [ 
+                "The Lazy Song"
+            ]
+        }, 
+        {
+            "singer" : "cold play",
+            "album" : [ 
+                "Viva la Vida"
+            ],
+            "song" : [ 
+                "Viva la Vida"
+            ]
+        }
+    ],
+    "lastVist" : ISODate("2017-07-20T19:40:50.615Z")
+}
+
+/* 11 */
+{
+    
+    "first_name" : "Larssen",
+    "last_name" : "Williams",
+    "user_name" : "larssen123",
+    "password" : "qazwsx123",
+    "age" : 24.0,
+    "city" : "New York",
+    "music_tags" : [ 
+        "rock", 
+        "pop", 
+        "classical", 
+        "piano"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "Justin Biber",
+            "album" : [ 
+                "Purpose"
+            ],
+            "song" : [ 
+                "what do you mean", 
+                "I will show you"
+            ]
+        }, 
+        {
+            "singer" : "Mozzart",
+            "song" : [ 
+                "symphone No.40", 
+                "piano sonata No.16", 
+                "oboe concerto"
+            ]
+        }, 
+        {
+            "singer" : "Maksim Mrvica",
+            "album" : [ 
+                "The Piano Player", 
+                "Pure II"
+            ],
+            "song" : [ 
+                "Exodus", 
+                "Tango in Ebony"
+            ]
+        }, 
+        {
+            "singer" : "Dorothy Ashby",
+            "album" : "the juzz harpist"
+        }
+    ],
+    "lastVist" : ISODate("2017-02-20T19:40:50.615Z")
+}
+
+/* 12 */
+{
+    
+    "first_name" : "Neil",
+    "last_name" : "Johns",
+    "user_name" : "NJisme",
+    "password" : "12345678",
+    "age" : 20.0,
+    "city" : "Miami",
+    "music_tags" : [ 
+        "rock"
+    ],
+    "favor" : [],
+    "lastVist" : ISODate("2010-01-28T09:40:50.615Z")
+}
+
+/* 13 */
+{
+    
+    "first_name" : "Obama",
+    "last_name" : "Barack",
+    "user_name" : "pasttense",
+    "password" : "12345678",
+    "age" : 55.0,
+    "city" : "Honolulu",
+    "music_tags" : [ 
+        "juzz", 
+        "blues"
+    ],
+    "favor" : [],
+    "lastVist" : ISODate("2010-11-28T09:40:50.615Z")
+}
+
+/* 14 */
+{
+    "first_name" : "Penny",
+    "last_name" : "Smith",
+    "user_name" : "1cent1",
+    "password" : "12345678",
+    "age" : 35.0,
+    "city" : "Maui",
+    "music_tags" : [ 
+        "juzz", 
+        "blues", 
+        "classical", 
+        "piano"
+    ],
+    "favor" : [],
+    "lastVist" : ISODate("2012-11-28T09:40:50.615Z")
+}
+
+/* 15 */
+{
+    
+    "first_name" : "Quency",
+    "last_name" : "Jackson",
+    "user_name" : "queenJ",
+    "password" : "12345678",
+    "age" : 13.0,
+    "city" : "Dallas",
+    "music_tags" : [],
+    "favor" : [],
+    "lastVist" : ISODate("2016-11-28T09:40:50.615Z")
+}
+
+/* 16 */
+{
+    
+    "first_name" : "Mathew",
+    "last_name" : "Williams",
+    "user_name" : "mathlover123",
+    "password" : "qazwsx123",
+    "age" : 29.0,
+    "city" : "New York",
+    "music_tags" : [ 
+        "rock"
+    ],
+    "favor" : [ 
+        {
+            "singer" : "Michael Jackson",
+            "song" : [ 
+                "Beat it"
+            ]
+        }
+    ],
+    "lastVist" : ISODate("2010-02-20T19:40:50.615Z")
+})
