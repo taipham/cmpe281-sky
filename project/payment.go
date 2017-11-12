008897dadd02b45467a59c41dfe407d774f55523
Hoang Nguyen <hoang1127@gmail.com>
2017-11-11 23:07:50 -0800

    Update Payment UI

--- project/payment.go
+++ project/payment.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import ("fmt" 
+        "net/http")
+
+func indexHandler(w http.ResponseWriter, r * http.Request) {
+    fmt.Fprintf(w, "<h1> Payment page </h1>")
+}
 
 func main() {
    fmt.Println("Payment")
+    http.HandleFunc("/", indexHandler)
+    http.ListenAdnServe(":8000", nil)
 }
