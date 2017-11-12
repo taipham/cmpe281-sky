826b7c59be364f8cebec8947ca87d536b47caa31
Hoang Nguyen <hoang1127@gmail.com>
2017-11-11 23:41:29 -0800

    Add Payment UI html

--- project/payment.go
+++ project/payment.go
@@ -1,14 +1,28 @@
+// Source : https://pythonprogramming.net/go/introduction-go-language-programming-tutorial/
+
 package main
 
 import ("fmt" 
-        "net/http")
+        "net/http"
+	"html/template")
 
+type NewsAggPage struct {
+	Title string
+	News string
+}
 func indexHandler(w http.ResponseWriter, r * http.Request) {
     fmt.Fprintf(w, "<h1> Payment page </h1>")
+}
+
+func PaymentHandler(w http.ResponseWriter, r * http.Request) {
+    p := NewsAggPage{Title:"Payment Process", News :"Shopping cart"}   
+    temp, _ := template.ParseFiles("paymentui.html")
+    temp.Execute(w, p)
 }
 
 func main() {
    fmt.Println("Payment")
     http.HandleFunc("/", indexHandler)
-    http.ListenAdnServe(":8000", nil)
+    http.HandleFunc("/payment", PaymentHandler)
+    http.ListenAndServe(":8000", nil)
 }
