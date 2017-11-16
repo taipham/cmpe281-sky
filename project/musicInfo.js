10b5df595fc4a47dc42e57d6f6077fe7260fed76
Tyler Nicholson <tan.hombre@gmail.com>
2017-11-16 07:16:24 -0800

    added musicInfo.js for mongodb

---
+++ project/musicInfo.js
@@ -0,0 +1,41 @@
+db.musicInfo.insert(
+
+{
+
+   "album" : "reputation",
+   "artist" : "Taylor Swift",
+   "songs" : 
+   [
+     "...Ready For It?",
+     "I Did Something Bad",
+     "Don't Blame Me"
+   ]
+},
+
+{
+
+   "album" : "The Beautiful & Damned",
+   "artist" : "G-Eazy",
+   "songs" : 
+   [
+     "The Beautiful & Damned (feat. Zoe Nash)",
+     "No Limit (feat. A$AP Rocky & Cardi B)",
+     "The Plan"
+   ]
+
+},
+
+{
+
+   "album" : "I Don't Believe We've Met",
+   "artist" : "Danielle Bradbery",
+   "songs" : 
+   [
+     "Sway",
+     "Worth It",
+     "Hello Summer"
+   ]
+
+}
+
+)
