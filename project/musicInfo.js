781a7721c4b562d61620c3bda5d2c906e5a62572
Melodyxman <MelodyXman@users.noreply.github.com>
2017-11-18 21:51:16 -0800

    Add files via upload

---
+++ project/musicInfo.js
@@ -0,0 +1,127 @@
+db.musicInfo.insert(
+[
+{
+   "artist" : "Taylor Swift",
+   "albums" : [ 
+   				{ "title": "reputation",
+   				  "songs": [
+                                                { "title:": "...Ready For It?","length": "3:12"},
+     						{ "title:": "I Did Something Bad","length": "4:24"},
+     						{ "title:": "Don't Blame Me", "length": "3:57"},
+     						{ "title:": "21","length": "4:21" }],
+     			   "description": "\n\tTaylor Alison Swift is an American singer-songwriter. One of the leading contemporary recording artists, she is known for narrative songs about her personal life, which have received widespread media coverage.\n    ",
+     			   "price": 23,
+     			   "iamge": "https://images.genius.com/5c8131af7d5d20f3c8f296538af74308.1000x1000x1.jpg"
+   				}],
+},
+{
+	"artist": "G-Eazy",
+	"albums": [
+				{ "title": "The Beautiful & Damned",
+				  "songs": [
+				  			{ "title": "The Beautiful & Damned (feat. Zoe Nash)", "length": "3:23"},
+				  			{ "title": "No Limit (feat. A$AP Rocky & Cardi B)", "length": "4:41" },
+				  			{ "title": "The Plan","length": "5:11"}
+                                            ],
+
+			  }]
+ 
+},
+{
+    "artist": "Danielle Bradbery",
+	"albums": [
+				{ "title": "I Don't Believe We've Met",
+				  "songs": [
+				  			{ "title": "Sway", "length": "5:43"},
+				  			{ "title": "Worth It", "length": "4:22" },
+				  			{ "title": "Hello Summer","length": "5:11"}
+                                                        
+                                            ],
+				  "description": "\n\tThis is awesome!\n",
+				  "price" : 29,
+				  "iamge": "http://yemphoto.com/blog/wp-content/uploads/2013/01/WE-DID-IT.jpg"
+				 }
+                             ]
+},
+
+{ "artist": "Radiohead",
+  "albums": [
+	 			 { "title":"The King of Limbs",
+	 			  "songs":[
+	 		 				{ "title":"Bloom","length":"5:15"},
+	 		 		        { "title":"Morning Mr Magpie","length":"4:41"},
+	 		 		        { "title":"Little by Little", "length":"4:27"},
+	 		 		        { "title":"Feral","length":"3:13"},
+	 		 		        { "title":"Lotus Flower","length":"5:01"},
+	 		 		        { "title":"Codex","length":"4:47"},
+	 		 		        { "title":"Give Up the Ghost","length":"4:50"},
+	 		 		        { "title":"Separator","length":"5:20"}
+                                           ],
+	 		 	  "description": "\n\tThe King of Limbs is the eighth studio album by English rock band Radiohead, produced by Nigel Godrich. It was self-released on 18 February 2011 as a download in MP3 and WAV formats, followed by physical CD, a wider digital release via AWAL, and a special newspaper edition on 9 May 2011. The physical editions were released through the band's Ticker Tape imprint on XL in the United Kingdom, TBD in the United States, and Hostess Entertainment in Japan.\n",
+	 		 	  "price": 20,
+	 		 	  "iamge": "https://en.wikipedia.org/wiki/The_King_of_Limbs#/media/File:The_king_of_limbs.jpg"
+	 		 	
+	 		 	 },
+	 		 	 { "title": "OK Computer",
+	 		 	  "songs": [
+	 		 	  			{ "title":"Airbag","length":"4:44"},
+	 		 	  			{ "title":"Paranoid Android","length":"6:23"},
+	 		 	  			{ "title":"Subterranean Homesick Alien","length":"4:27"},
+	 		 	  			{ "title":"Exit Music (For a Film)","length":"4:24"},
+	 		 	  			{ "title":"Let Down","length":"4:59"},
+	 		 	  			{ "title":"Karma Police","length":"4:21"},
+	 		 	  			{ "title":"Fitter Happier","length":"1:57"},
+	 		 	  			{ "title":"Electioneering","length":"3:50"},
+	 		 	  			{ "title":"Climbing Up the Walls","length":"4:45"},
+	 		 	  			{ "title":"No Surprises","length":"3:48"},
+	 		 	  			{ "title":"Lucky","length":"4:19"},
+	 		 	  			{ "title":"The Tourist","length":"5:24"}
+	 		 	  			],
+	 		 	  "description": "\n\tOK Computer is the third studio album by the English alternative rock band Radiohead, released on 16 June 1997 on Parlophone in the United Kingdom and 1 July 1997 by Capitol Records in the United States. It marks a deliberate attempt by the band to move away from the introspective guitar-oriented sound of their previous album The Bends. Its layered sound and wide range of influences set it apart from many of the Britpop and alternative rock bands popular at the time and laid the groundwork for Radiohead's later, more experimental work.\n      ",
+	 		 	  "price": 15,
+	 		 	  "image": "https://upload.wikimedia.org/wikipedia/en/a/a1/Radiohead.okcomputer.albumart.jpg"
+	 		 	}
+	 		 	]
+},
+{ "artist": "Portishead",
+  "albums": [
+	  			{ "title": "Roseland NYC Live",
+	  			  "songs": [
+	  				  			{ "title":"Mysterons","length":"5:02"},
+	  				  			{ "title":"Sour Times","length":"4:11"},
+	  				  			{ "title":"Strangers","length":"3:55"},
+	  				  			{ "title":"It Could Be Sweet","length":"4:16"},
+	  				  			{ "title":"Wandering Star","length":"4:51"}, 
+	  				  			{ "title":"It's a Fire","length":"3:49"},
+	  				  			{ "title":"Numb","length":"3:54"},
+	  				  			{ "title":"Roads","length":"5:02"},
+	  				  			{ "title":"Pedestal","length":"3:39"},
+	  				  			{ "title":"Biscuit","length":"5:01"},
+	  				  			{ "title":"Glory Box","length":"5:06"}
+	  				  		   ],
+	  				  "description":"\n\tDummy is the debut album of the Bristol-based group Portishead. Released in August 22, 1994 on Go! Discs, the album earned critical acclaim, winning the 1995 Mercury Music Prize. It is often credited with popularizing the trip-hop genre and is frequently cited in lists of the best albums of the 1990s. Although it achieved modest chart success overseas, it peaked at #2 on the UK Album Chart and saw two of its three singles reach #13. The album was certified gold in 1997 and has sold two million copies in Europe. As of September 2011, the album was certified double-platinum in the United Kingdom and has sold as of September 2011 825,000 copies.\n      ",
+	  				  "price": 17,
+	  				  "iamge": "https://en.wikipedia.org/wiki/Roseland_NYC_Live#/media/File:Pnyc.jpg"
+
+	  			 },
+	  			{ "title": "Third",
+	  			  "songs": [
+	  				  			{ "title":"Silence","length":"4:58"},
+	  				  			{ "title":"Hunter","length":"3:57"},
+	  				  			{ "title":"Nylon Smile","length":"3:16"},
+	  				  			{ "title":"The Rip","length":"4:29"}, 
+	  				  			{ "title":"Plastic","length":"3:27"},
+	  				  			{ "title":"We Carry On","length":"6:27"},
+	  				  			{ "title":"Deep Water","length":"1:31"},
+	  				  			{ "title":"Machine Gun","length":"4:43"}, 
+	  				  			{ "title":"Small","length":"6:45"},
+	  				  			{ "title":"Magic Doors","length":"3:32"},
+	  				  			{ "title":"Threads","length":"5:45"}
+	  				  			],
+	  				  "description": "\n\tThird is the third studio album by English musical group Portishead, released on 27 April 2008, on Island Records in the United Kingdom, two days after on Mercury Records in the United States, and on 30 April 2008 on Universal Music Japan in Japan. It is their first release in 10 years, and their first studio album in eleven years. Third entered the UK Album Chart at #2, and became the band's first-ever American Top 10 album on the Billboard 200, reaching #7 in its entry week.\n      ",
+	  				  "price": 18,
+	  				  "iamge": "https://en.wikipedia.org/wiki/Third_(Portishead_album)#/media/File:Portishead_-_Third.png"
+	  			}]
+                              
+}])
+
