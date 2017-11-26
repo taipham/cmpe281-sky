a1767ee7e86d7879589d4a74aafabe21240bbffa
hoang1127 <hoang1127@gmail.com>
2017-11-25 23:26:47 -0800

    Update Payment

--- project/paymentdb.go
+++ project/paymentdb.go
@@ -1,9 +1,54 @@
 package main
 
 import( "log"
-	"net/http")
+		"net/http"
+		"time"
+		"fmt"
+		"gopkg.in/mgo.v2"
+		"gopkg.in/mgo.v2/bson"
+	)
 
+type Song struct {
+	songID		uint8
+	songName	string
+	singerName	string
+	musicGroup	string
+	albumName	string
+	price		float32
+
+}
+
+type Order struct {
+	orderID			uint8
+	createDate		string
+	customerName	string
+	customerID		uint8
+	songID			uint8
+	songName		string
+	totalCost		float32
+	statusOrder		string
+}
 func main() {
+
+	Host := []string{
+			"127.0.0.1:8000" }
+			const (
+				Username = "cmpe281"
+				Password = "fall"
+				Database = "musicPayment_DB"
+				Collection = "payment_process"
+			)
+			session, err := mgo.DialWithInfo(&mgo.DialWithInfo{
+				Addrs: Host,
+
+			})
+			if err != nil {
+				panic(err)
+			}
+
+			defer session.Close()
+
+			
 	router :=productdb.NewRouter()
 
 	m_origins := handlers.AllowedOrigins([]string{"*"}) 
